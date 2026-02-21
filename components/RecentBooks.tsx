568ae63439882fb52566b26a26d17bdd521751a3
daoui2006 <hamiddaoui2006@gmail.com>
2026-02-21 11:47:19 +0000

    Create RecentBooks component

---
+++ components/RecentBooks.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const RecentBooks = () => {
+    return (
+        <div>
+            <h2>Recent Books</h2>
+            {/* Add your recent books data here */}
+            <ul>
+                <li>Book 1</li>
+                <li>Book 2</li>
+                <li>Book 3</li>
+            </ul>
+        </div>
+    );
+};
+
+export default RecentBooks;
